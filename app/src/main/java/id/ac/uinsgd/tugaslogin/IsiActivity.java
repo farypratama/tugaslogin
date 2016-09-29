bb95603216921011eb47ce84b6394878bc4a3348
Muhamad Fary Pratama <farypratama@gmail.com>
2016-09-29 15:17:46 +0700

    Add isi, register, webview

--- app/src/main/java/id/ac/uinsgd/tugaslogin/IsiActivity.java
+++ app/src/main/java/id/ac/uinsgd/tugaslogin/IsiActivity.java
@@ -1,7 +1,9 @@
 package id.ac.uinsgd.tugaslogin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class IsiActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class IsiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_isi);
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.loadUrl("http://m.facebook.com");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 }
--- app/src/main/java/id/ac/uinsgd/tugaslogin/LoginActivity.java
+++ app/src/main/java/id/ac/uinsgd/tugaslogin/LoginActivity.java
@@ -1,13 +1,62 @@
 package id.ac.uinsgd.tugaslogin;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btn_login,btn_register;
+    EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }   
+        btn_login=(Button)findViewById(R.id.btn_login);
+        btn_register=(Button)findViewById(R.id.btn_register);
+        username=(EditText)findViewById(R.id.txt_username);
+        password=(EditText)findViewById(R.id.txt_password);
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((username.getText().toString().equals("admin"))&&(password.getText().toString().equals("admin")))
+                {
+                    startActivity(new Intent(LoginActivity.this,IsiActivity.class));
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Username dan Password Salah", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+    }
+
+    public void LoginFacebook(View view)
+    {
+
+    }
+    public void LoginGoogle(View view)
+    {
+
+    }
+    public void LoginTwitter(View view)
+    {
+
+    }
+
 }
